b5da909d048e97294fe02e711f57afb4c7c9867c
Giovanni Coppola <5774411+giovannicoppola@users.noreply.github.com>
2023-03-10 17:00:01 -0500

    Update fetchMeetingDetails.py

--- fetchMeetingDetails.py
+++ fetchMeetingDetails.py
@@ -23,8 +23,6 @@ myTimeStart = round(time.time())
 scpt = '''
     on run {minInterval}
 
-# Original script from user https://www.reddit.com/user/scrutinizer1/ from this thread https://www.reddit.com/r/applescript/comments/mep684/is_it_easy_to_pull_content_from_an_outlook/
-
 
 with timeout of (2800 * minutes) seconds
 	
@@ -46,57 +44,10 @@ with timeout of (2800 * minutes) seconds
 		repeat with CalEv in (get every calendar event whose start time is less than or equal to now5 and end time is greater than now5)
 			set the clipboard to ""
 			tell CalEv
-                set theTitles to theTitles & "--" & (subject as text) 
-				if (all day flag) then
-					set AllDay to "Yes"
-				else
-					set AllDay to "No"
-				end if
-				if has reminder then
-					set HasRem to "Yes"
-					set RemOn to "Minutes until fires up: " & (reminder time) as text
-				else
-					set HasRem to "No"
-					set RemOn to ""
-				end if
-				if is recurring then
-					set IsRecur to "Yes"
-					set Recur to recurrence
-					set RecurID to recurrence id
-				else
-					set IsRecur to "No"
-					set Recur to ""
-					set RecurID to "N/A"
-				end if
-				if is occurrence then
-					set IsOcur to "Yes"
-				else
-					set IsOcur to "No"
-				end if
-				if request responses then
-					set Requests to "Yes"
-				else
-					set Requests to "No"
-				end if
-				
-				
-				
-				set |Attendees| to ""
-				set _Attendees to attendees
-				if _Attendees is not {} then
-					repeat with i from 1 to the number of items in _Attendees
-						set PersonAttends to _Attendees's item i
-						tell PersonAttends
-							set |Attendees| to (((|Attendees| & "Email: " & (email address) as text) & ", " & "Attendee type: " & (type) as text) & ", " & "Status: " & status as text) & return
-						end tell
-					end repeat
-				end if
-				
-				set CalEvProperties to (((((("Event" & id as text) & " info" & return & return & "Subject: " & (subject as text) & "," & return & "Starts: " & (start time) as text) & "," & space & "Ends: " & (end time) as text) & "," & space & "All day: " & AllDay & "," & return & "Free-busy: " & (free busy status) as text) & ", " & return & "Reminder set: " & HasRem & ", " & RemOn & return & "Organized by: " & organizer & ", " & return & "Recurring: " & IsRecur & ", " & "Recurrence: " & Recur & ", " & "Recurrence ID: " & RecurID & ", " & return & "Is event occurrence: " & IsOcur & ", " & return & "Requests responses: " & Requests & ", " & "Event description: " & (plain text content) & ", " & return & "Last modified: " & (modification date) as text) & ", " & return & "Location: " & location & "TMZ: " & (timezone) as text) & "," & return & return & "_______________" & return & return & "Attendees: " & return & return & |Attendees|
-				set the clipboard to CalEvProperties
-				repeat until (the clipboard) contains ("Event" & id as text) & " info"
-					delay 0.1
-				end repeat
+                
+				# repeat until (the clipboard) contains ("Event" & id as text) & " info"
+				# 	delay 0.1
+				# end repeat
 				
                 
                 
@@ -139,7 +90,7 @@ end run
 '''
 
 #args = [SPRINT_DUR, Email_Start, Email_StartF, sprintDurSec]
-args = ["-383"]        
+args = ["5"]        
 p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 stdout, stderr = p.communicate(scpt)
 print (stdout)
